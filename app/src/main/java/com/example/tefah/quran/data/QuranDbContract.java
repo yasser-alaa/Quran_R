f3d4a570f625e86463f3bb7c4af8f3f28d9ddd1e
yasseralaa <mryasser159@hotmail.com>
2018-06-20 11:46:07 +0200

    database done

--- app/src/main/java/com/example/tefah/quran/data/QuranDbContract.java
+++ app/src/main/java/com/example/tefah/quran/data/QuranDbContract.java
@@ -1,8 +1,18 @@
 package com.example.tefah.quran.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by yasseralaa on 15/05/18.
  */
 
 public class QuranDbContract {
+    public static final class QuranEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Quran";
+        public static final String COLUMN_SURA_ID = "SuraID";
+        public static final String COLUMN_VERSE_ID = "VerseID";
+        public static final String COLUMN_VERSE_TEXT = "AyahText";
+        public static final String COLUMN_SURA_NAME = "SuraName";
+
+    }
 }
--- app/src/main/java/com/example/tefah/quran/data/QuranDbHelper.java
+++ app/src/main/java/com/example/tefah/quran/data/QuranDbHelper.java
@@ -1,8 +1,50 @@
 package com.example.tefah.quran.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by yasseralaa on 15/05/18.
  */
 
-public class QuranDbHelper {
+public class QuranDbHelper extends SQLiteOpenHelper {
+
+    //database name
+    private static final String DATABASE_NAME = "Quran.db";
+
+    // If you change the database schema, you must increment the database version
+    private static final int DATABASE_VERSION = 3;
+
+
+    public QuranDbHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_QURAN_TABLE = "CREATE TABLE " + QuranDbContract.QuranEntry.TABLE_NAME + " (" +
+                QuranDbContract.QuranEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuranDbContract.QuranEntry.COLUMN_SURA_ID + " INTEGER NOT NULL, " +
+                QuranDbContract.QuranEntry.COLUMN_VERSE_ID + " INTEGER NOT NULL, " +
+                QuranDbContract.QuranEntry.COLUMN_VERSE_TEXT + " TEXT NOT NULL, "+
+                QuranDbContract.QuranEntry.COLUMN_SURA_NAME + " TEXT NOT NULL "+
+
+                "); ";
+
+        //EXcute qurey
+        sqLiteDatabase.execSQL(SQL_CREATE_QURAN_TABLE);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //iNSTER HERE
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + QuranDbContract.QuranEntry.TABLE_NAME);
+
+        onCreate(sqLiteDatabase);
+    }
 }
+
