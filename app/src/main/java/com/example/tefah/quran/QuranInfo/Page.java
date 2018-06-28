00690690c847add40d024ccd0d9b7d2c76120d58
yasseralaa <mryasser159@hotmail.com>
2018-06-28 17:43:49 +0200

    show quran like in moshafe from fateha

---
+++ app/src/main/java/com/example/tefah/quran/QuranInfo/Page.java
@@ -0,0 +1,8 @@
+package com.example.tefah.quran.QuranInfo;
+
+/**
+ * Created by yasseralaa on 27/06/18.
+ */
+
+public class Page {
+}
---
+++ app/src/main/java/com/example/tefah/quran/QuranInfo/Sura.java
@@ -0,0 +1,8 @@
+package com.example.tefah.quran.QuranInfo;
+
+/**
+ * Created by yasseralaa on 26/06/18.
+ */
+
+public class Sura {
+}
